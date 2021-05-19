b4829f349d46cc9e7ed1043ff54337a4b9402c4f
saracha <saracha06422@gmail.com>
2021-05-19 22:37:30 +0700

    add-map

--- indoor_webapp.php
+++ indoor_webapp.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="room_style.css" type="text/css">
     <title>Document</title>
 </head>
 <body>
@@ -18,32 +19,71 @@
     
     <h3 class="room">Position : <?php echo $room; ?></h3>
 
-    <figure class="snip2">
-        <img src="xbee3.jpg" alt="">
-        <figcaption>
-            <h3 class="title1">Xbee1</h3>
-            <h3 class="title2"><?php echo $router1; ?></h3>
-            <h3 class="title3">router1</h3>
-        </figcaption>
-        <a href="#"></a>
-    </figure>
-    <figure class="snip2">
-        <img src="xbee3.jpg" alt="">
-        <figcaption>
-            <h3 class="title1">Xbee2</h3>
-            <h3 class="title2"><?php echo $router2; ?></h3>
-            <h3 class="title3">router2</h3>
-        </figcaption>
-        <a href="#"></a>
-    </figure>
-    <figure class="snip2">
-        <img src="xbee3.jpg" alt="">
-        <figcaption>
-            <h3 class="title1">Xbee3</h3>
-            <h3 class="title2"><?php echo $coor; ?></h3>
-            <h3 class="title3">coordinator</h3>
-        </figcaption>
-        <a href="#"></a>
-    </figure>
+
+    <div class="boxroom">
+        <div class="room_1">
+            <h2>ROOM A</h2>
+            <?php 
+            if ( $room == 'Room A'){
+                echo '<div class="center">
+                <span class="pointer"></span>
+                <span class="pulse"></span>
+                </div>';
+            }
+            ?>
+        </div>
+        <div class="room_2">
+            <h2>ROOM B</h2>
+            <?php 
+                if ( $room == 'Room B'){
+                    echo '<div class="center">
+                    <span class="pointer"></span>
+                    <span class="pulse"></span>
+                    </div>';
+                }
+            ?>
+        </div>
+        <div class="room_3">
+            <h2>ROOM C</h2>
+            <?php 
+                if ( $room == 'Room C'){
+                    echo '<div class="center">
+                    <span class="pointer"></span>
+                    <span class="pulse"></span>
+                    </div>';
+                }
+            ?>
+        </div>
+    </div>
+    <div class="dis">
+        <figure class="snip2">
+            <img src="xbee3.jpg" alt="">
+            <figcaption>
+                <h3 class="title1">Xbee1</h3>
+                <h3 class="title2"><?php echo $router1; ?></h3>
+                <h3 class="title3">router1</h3>
+            </figcaption>
+            <a href="#"></a>
+        </figure>
+        <figure class="snip2">
+            <img src="xbee3.jpg" alt="">
+            <figcaption>
+                <h3 class="title1">Xbee2</h3>
+                <h3 class="title2"><?php echo $router2; ?></h3>
+                <h3 class="title3">router2</h3>
+            </figcaption>
+            <a href="#"></a>
+        </figure>
+        <figure class="snip2">
+            <img src="xbee3.jpg" alt="">
+            <figcaption>
+                <h3 class="title1">Xbee3</h3>
+                <h3 class="title2"><?php echo $coor; ?></h3>
+                <h3 class="title3">coordinator</h3>
+            </figcaption>
+            <a href="#"></a>
+        </figure>
+    </div>
+   
 </body>
 </html>
--- profile.php
+++ profile.php
@@ -13,7 +13,7 @@
         <figure class="snip">
             <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
             <figcaption>
-                <img src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t1.6435-9/82566883_3027448490686637_8324559532126633984_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=174925&_nc_eui2=AeEVGxUqWvVEPxg8JLbOGblcvd0yEo7WVWS93TISjtZVZEEreSpJgukF9gCx195wS7rKn3IKfXvcUSZH8qR-imaA&_nc_ohc=IO0mi7pboUMAX8cAr02&_nc_ht=scontent.fbkk22-6.fna&oh=28ed101f16515d028512b0b7bdc62371&oe=609E4D98" alt="profile-img" class="profile">
+                <img src="dew.jpg" alt="profile-img" class="profile">
                 <h2>Saracha sangsuemoon <span>Developer</span></h2>           
                 <p> Introduce myself<br>
                    - Mr.Saracha Sangsuemoon.<br>
@@ -29,7 +29,7 @@
         <figure class="snip">
             <img src="https://images.pexels.com/photos/257736/pexels-photo-257736.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
             <figcaption>
-                <img src="https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.6435-9/123328606_3294903050607355_4969257163244915011_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHMkTmYP1Syv0zlnjXFBS9TGj-q1LQJe2QaP6rUtAl7ZBQP1KPTRYAJgcGejDMREgq136kobQIY8RcZv7rjOiuG&_nc_ohc=a-d6phoo62oAX_pukWg&_nc_ht=scontent.fbkk5-3.fna&oh=fc77afc2129923ca1a0636876b04bcbb&oe=609D7054" alt="profile-img" class="profile">
+                <img src="fa.jpg" alt="profile-img" class="profile">
                 <h2>Phitchayut kalong <span>Technician/Tester</span></h2>            
                 <p> Introduce myself<br>
                    - Mr.phitchayut kalong.<br>
